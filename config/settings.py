f6f3f5d5dc2cbf7926dc8602f152bead465bbd93
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2021-11-30 12:26:59 -0500

    removed sqllite made migrations on heroku

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_on_heroku
 django_on_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
