b0ac24ee15a03b692bb1c9661157fd7498c30c5a
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2021-12-14 10:27:01 -0500

    add django cors headers

--- config/settings.py
+++ config/settings.py
@@ -58,9 +58,12 @@ INSTALLED_APPS = [
     'habittracker',
     'api',
     'djoser',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -167,3 +170,5 @@ REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': 
         ['rest_framework.authentication.TokenAuthentication'],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
